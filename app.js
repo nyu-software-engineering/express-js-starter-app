fd065e29460da44245dbffdb1bc6800e217abd44
bloombar <amos@cs.nyu.edu>
2021-10-28 23:33:46 -0400

    fixing uploaded filenames

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
+const path = require("path")
 
 // import some useful middleware
 const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
@@ -26,7 +27,7 @@ app.use("/static", express.static("public"))
 
 // route for HTTP GET requests to the root document
 app.get("/", (req, res) => {
-  res.send("Hello!")
+  res.send("Goodbye world!")
 })
 
 // route for HTTP GET requests to /html-example
@@ -96,7 +97,13 @@ const storage = multer.diskStorage({
     cb(null, "public/uploads")
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now())
+    // take apart the uploaded file's name so we can create a new one based on it
+    const extension = path.extname(file.originalname)
+    const basenameWithoutExtension = path.basename(file.originalname, extension)
+    // create a new filename with a timestamp in the middle
+    const newName = `${basenameWithoutExtension}-${Date.now()}${extension}`
+    // tell multer to use this new filename for the uploaded file
+    cb(null, newName)
   },
 })
 const upload = multer({ storage: storage })
@@ -131,12 +138,19 @@ app.get("/proxy-example", (req, res, next) => {
 })
 
 // same route as above, but using environmental variables for secret credentials
-app.get("/dotenv-example", (req, res, next) => {
-  // insert the environmental variable into the URL we're requesting
-  axios
-    .get(`${process.env.API_BASE_URL}?key=${process.env.API_SECRET_KEY}&num=10`)
-    .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
-    .catch(err => next(err)) // pass any errors to express
+app.get("/dotenv-example", async (req, res, next) => {
+  try {
+    // insert the environmental variable into the URL we're requesting
+    const response = await axios.get(
+      `${process.env.API_BASE_URL}?key=${process.env.API_SECRET_KEY}&num=10`
+    )
+    res.json(response.data) // pass data along directly to client
+  } catch (err) {
+    res.json({
+      success: false,
+      error: `Oops... In order to use the dotenv module, you must first make a file named .env on your server - see the .env.example file for example.`,
+    })
+  }
 })
 
 // a route with parameter ... animalId is a parameter
@@ -145,22 +159,25 @@ app.get("/dotenv-example", (req, res, next) => {
 app.get("/parameter-example/:animalId", async (req, res) => {
   // use axios to make a request to an API to fetch a single animal's data
   // we use a Mock API here, but imagine we passed the animalId to a real API and received back data about that animal
-  const apiResponse = await axios
-    .get(
+  try {
+    const apiResponse = await axios.get(
       `${process.env.API_BASE_URL}?key=${process.env.API_SECRET_KEY}&num=1&id=${req.params.animalId}`
     )
-    .catch(err => next(err)) // pass any errors to express
 
-  // express places parameters into the req.params object
-  const responseData = {
-    status: "wonderful",
-    message: `Imagine we got the data from the API for animal #${req.params.animalId}`,
-    animalId: req.params.animalId,
-    animal: apiResponse.data,
+    // express places parameters into the req.params object
+    const responseData = {
+      status: "wonderful",
+      message: `Imagine we got the data from the API for animal #${req.params.animalId}`,
+      animalId: req.params.animalId,
+      animal: apiResponse.data,
+    }
+
+    // send the data in the response
+    res.json(responseData)
+  } catch (err) {
+    // send an error JSON object back to the browser
+    res.json(err)
   }
-
-  // send the data in the response
-  res.json(responseData)
 })
 
 // export the express app we created to make it available to other modules
